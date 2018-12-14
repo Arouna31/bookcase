65c7cbca5d93f273099912f7f4611f31af20c5cf
Arouna31 <arounakinda@gmail.com>
2018-12-14 17:44:37 +0000

    creation of app components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SingupComponent } from './auth/singup/singup.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { SingleBookComponent } from './books-list/single-book/single-book.component';
+import { BookFormComponent } from './books-list/book-form/book-form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingupComponent,
+    SignupComponent,
+    SigninComponent,
+    BooksListComponent,
+    SingleBookComponent,
+    BookFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
