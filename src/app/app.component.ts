6f14ad087701135158b35feabcfe232d05b20cf2
Arouna31 <arounakinda@gmail.com>
2018-12-14 19:07:35 +0000

    bind app with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookcase';
+
+  constructor() {
+
+    var config = {
+      apiKey: "AIzaSyA4e0sRmlG0i-51Wm2m74Xha4uJz7vVagw",
+      authDomain: "bookcase-36b01.firebaseapp.com",
+      databaseURL: "https://bookcase-36b01.firebaseio.com",
+      projectId: "bookcase-36b01",
+      storageBucket: "",
+      messagingSenderId: "709763908044"
+    };
+    firebase.initializeApp(config);
+
+  }
 }
